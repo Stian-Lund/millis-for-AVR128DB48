4caf065d80850fe4b3a80de379cf8e1498e97056
Stian-Lund <69124213+Stian-Lund@users.noreply.github.com>
2025-10-15 01:03:58 +0200

    Add files via upload

---
+++ millis.c
@@ -0,0 +1,44 @@
+#include "millis.h"
+#include <util/atomic.h>
+
+#ifndef F_CPU
+#warning "F_CPU not defined! Assuming 4MHz"
+#define F_CPU 4000000UL
+#endif
+
+#if (F_CPU / 1000UL) > 65535UL
+#error "F_CPU too high for DIV1 prescaler, use a higher prescaler"
+#endif
+
+volatile uint32_t milliseconds = 0;
+
+void millis_init(void)
+{
+    // Stop timer
+    TCA0.SINGLE.CTRLA = 0;
+
+    // Configure TCA0 for 1ms overflow with F_CPU clock
+    TCA0.SINGLE.PER = (F_CPU / 1000UL) - 1; // 1000Hz
+    TCA0.SINGLE.CNT = 0;                    // Reset count
+
+    TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm; // Clear overflow interrupt flag
+    TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;  // Enable overflow interrupt
+
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_ENABLE_bm | TCA_SINGLE_CLKSEL_DIV1_gc; // Enable timer & set CLKSEL to DIV1
+}
+
+uint32_t millis(void)
+{
+    uint32_t ms;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        ms = milliseconds;
+    }
+    return ms;
+}
+
+ISR(TCA0_OVF_vect)
+{
+    milliseconds++;
+    TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm; // Clear interrupt flag
+}
---
+++ millis.h
@@ -0,0 +1,27 @@
+/*
+ * File:   millis.h
+ * Author: Stian
+ *
+ * Created on October 14, 2025, 9:36 PM
+ */
+
+#ifndef MILLIS_H
+#define MILLIS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+
+    void millis_init(void);
+    uint32_t millis(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MILLIS_H */
